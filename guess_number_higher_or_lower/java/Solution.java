3bbf01d221667e94cf84ec0d460f1e9fb7b0bf14
Doruk Kantarcıoğlu <63489557+DorukKantarcioglu@users.noreply.github.com>
2021-11-17 21:01:05 +0300

    Create Solution.java
    Simple binary search

---
+++ guess_number_higher_or_lower/java/Solution.java
@@ -0,0 +1,28 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is lower than the guess number
+ *			      1 if num is higher than the guess number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int left = 1;
+        int right = n;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (guess(mid) == 1) {
+                left = mid + 1;
+            }
+            else if (guess(mid) == -1) {
+                right = mid - 1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return 0;
+    }
+}
