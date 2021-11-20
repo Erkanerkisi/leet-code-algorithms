444f524bffde855f2e834784d89307a97f4537c7
Doruk Kantarcıoğlu <63489557+DorukKantarcioglu@users.noreply.github.com>
2021-11-20 19:48:56 +0300

    Create Solution.java
    Recursive, easy to understand.

---
+++ same_tree/java/Solution.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {   // Both nodes are null
+            return true;
+        }
+        else if (p == null || q == null) {  // Either p is null or q is null
+            return false;
+        }
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
